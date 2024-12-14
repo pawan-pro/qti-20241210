f8d8a229825a900dda62898f61879427a0dfd035
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2024-12-14 22:43:58 +0530

    20241214 update4 - google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         {/* Google Analytics */}
-        <script
-          async
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-WD3DDZ5171"
-        ></script>
-        <script
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
@@ -18,7 +21,7 @@ export default function Document() {
               gtag('config', 'G-WD3DDZ5171');
             `,
           }}
-        ></script>
+        />
       </Head>
       <body>
         <Main />
