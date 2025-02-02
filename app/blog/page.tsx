62faacab018f81542bc92c3de107fafc93a707a8
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2025-02-02 22:16:12 +0530

    20250202 update - 20250202 blog updated

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 const projects = [
   {
     id: 'blog20250202',  
-    title: 'Key Economic Events: Week Of 03rd Feb 2025',
+    title: 'Key Economic Events: Week Of 02nd Feb 2025',
     image: '/blogs/blog-cover/20250202.png',
     link: '/blogs/20250202.html', // blog html file
   }, 
