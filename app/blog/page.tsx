95be1bbccc07d29dd2ff4d598000b69ec24b077d
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2025-02-06 14:21:38 +0530

    e retrying:wq
    :wq
    
    :wq

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -135,8 +135,3 @@ export default function ResourcesPage() {
     </main>
   );
 }
-
-// git commands
-// git add .                                                                                                          
-// git commit -m "20250120 update - 20240118 blog added"
-// git push origin main
