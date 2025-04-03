87ad0ad2ce3b998144c21f93742fe0f2ba7cc8e0
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2025-04-03 19:41:02 +0530

    20250403: updating blog and adding 404 images 4

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,13 +6,17 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export default function NotFound() {
-  const [randomImage, setRandomImage] = useState('');
+  const [randomImage, setRandomImage] = useState<string>('');
 
   useEffect(() => {
     // Define the list of image paths
     const images = [
-      '/404/Business-Cat-giving-presentation-Red G.jpg','/404/Business-Cat-giving-presentation-Green G.jpg','/404/Business-Cat-giving-presentation-Blue G.jpg',
-      '/404/Business-Cat-giving-presentation-Red O.jpeg','/404/Business-Cat-giving-presentation-Green O.jpeg','/404/Business-Cat-giving-presentation-Blue O.jpeg',
+      '/404/Business-Cat-giving-presentation-Red G.jpg',
+      '/404/Business-Cat-giving-presentation-Green G.jpg',
+      '/404/Business-Cat-giving-presentation-Blue G.jpg',
+      '/404/Business-Cat-giving-presentation-Red O.jpeg',
+      '/404/Business-Cat-giving-presentation-Green O.jpeg',
+      '/404/Business-Cat-giving-presentation-Blue O.jpeg',
     ];
 
     // Randomly select an image
@@ -23,7 +27,7 @@ export default function NotFound() {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>404 - Page Not Found</h1>
-      <div className={styles.image}>
+      <div className={styles.imageWrapper}>
         {randomImage && (
           <Image
             src={randomImage} // Use the randomly selected image
@@ -34,8 +38,10 @@ export default function NotFound() {
         )}
       </div>
       <p className={styles.message}>
-        Oh no! It looks like the page you&apos;re looking for has gone on a coffee break with Business Cat.<br></br><br></br> 
-        While Business Cat is busy presenting the figures, we&apos;re working to find where this page has scampered off to. <br></br><br></br>
+        Oh no! It looks like the page you&apos;re looking for has gone on a coffee break with Business Cat.
+        <br /><br />
+        While Business Cat is busy presenting the figures, we&apos;re working to find where this page has scampered off to.
+        <br /><br />
         Please try another link or return to our homepage.
       </p>
       <Link href="/" className={styles.link}>
@@ -44,7 +50,3 @@ export default function NotFound() {
     </div>
   );
 }
-
-//git add .                                                                               
-//git commit -m "20250120 update - 20240120 blog added"
-//git push origin main
