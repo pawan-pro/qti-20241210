0d8f8c1765cbfbcccccd92a3f5da0f625fe5b75b
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2025-08-17 20:38:06 +0530

    20250818: adding blog 20250818

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,13 @@ import styles from './page.module.css';
 
 const projects = [
   {
+    id: 'blog20250818',
+    title: 'Key Economic Events: Week Of 18th August 2025',
+    image: '/blogs/blog-cover/20250818.png',
+    link: '/blogs/20250818.html',
+    //description: 'Exploring the impact of geopolitical events on global markets.',
+  },
+  {
     id: 'blog20250811',
     title: 'Key Economic Events: Week Of 11th August 2025',
     image: '/blogs/blog-cover/20250811.png',
