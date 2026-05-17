cc62c7a30283bc88131ef838c9f3eba0678b6aba
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2026-05-17 20:51:28 +0530

    Add May 18 economic calendar blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,12 @@ import styles from './page.module.css';
 
 const projects = [
   {
+    id: 'blog20260518',
+    title: 'Key Economic Events: Week Of 18th May 2026',
+    image: '/blogs/blog-cover/20260518.png',
+    link: '/blogs/20260518.html',
+  },
+  {
     id: 'blog20260510',
     title: 'Key Economic Events: Week Of 10th May 2026',
     image: '/blogs/blog-cover/20260510.png',
