81e3b9c3d0b0e0b4134349149b1da54673fe432e
pawan-professional <141957353+pawan-professional@users.noreply.github.com>
2026-06-14 21:47:15 +0530

    feat(blog): add weekly economic calendar for 15-Jun-2026

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,12 @@ import styles from './page.module.css';
 
 const projects = [
   {
+    id: 'blog20260615',
+    title: 'Key Economic Events: Week Of 15th June 2026',
+    image: '/blogs/blog-cover/20260615.png',
+    link: '/blogs/20260615.html',
+  },
+  {
     id: 'blog20260607',
     title: 'Key Economic Events: Week Of 7th June 2026',
     image: '/blogs/blog-cover/20260607.png',
